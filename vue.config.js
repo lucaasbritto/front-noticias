1a44af934486858f86c62227eac1f0e2f17ecaca
Lucas <lucaasbritto@github.com>
2022-11-26 18:49:38 -0300

    Alteracao de url backend

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  pathUrl: '/sistema/investidor10/noticias'
+  publicPath: '/sistema/investidor10/noticias'
 })
